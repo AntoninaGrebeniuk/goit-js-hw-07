348b4843b03641b421fef93c17c4a0afb9df3fbc
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-03-03 09:52:21 +0300

    add the second gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const makeGalleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
